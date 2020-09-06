e6408bcaebac4f433842572e919a120a20136651
Suyash <suyashukla@gmail.com>
2020-09-06 16:50:00 +0530

    Final Commit

---
+++ src/server/index.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io  =    require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.send('<h1>Hello World</h1>')
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('chat message', function(msg){
+        console.log('message: '+JSON.stringify(msg));
+    })
+});
+
+
+http.listen(3001, function(){
+    console.log('Listening on *: 3001');
+});
